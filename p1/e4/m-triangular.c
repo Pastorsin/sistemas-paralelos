09d6b05941687bb17a4ecd81c7b2ec8159ef784f
Andrés Milla <andressmilla@gmail.com>
2020-03-26 17:52:25 -0300

    Reemplacé BT por la matriz Prueba

--- p1/e4/m-triangular.c
+++ p1/e4/m-triangular.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int N=100;
+int N=3;
 
 
 //Para calcular tiempo
@@ -15,39 +15,44 @@ double dwalltime(){
 }
 
 //Para calcular la cantidad de 0 a restar
-int calcularCeros(int columna){
-        int ceros,tamMatriz,dimMatriz,cantElementos;
+int cantidad_ceros(int columna){
+  int ceros,tamMatriz,dimMatriz,cantElementos;
+
 	dimMatriz=columna+1;
 	tamMatriz=dimMatriz*dimMatriz;
-        cantElementos=dimMatriz*(dimMatriz+1)/2;
+  cantElementos=dimMatriz*(dimMatriz+1)/2;
 	ceros=tamMatriz-cantElementos;
-        return ceros;
+
+  return ceros;
 }
 
 int main(int argc,char* argv[]){
 
- double *A,*BT,*C,*Prueba;;
- int i,j,k,tamTriangular;
+ double *A,*BT,*C,*Prueba;
+ int i,j,k;
+
  int check = 1;
  double timetick;
 
- //Controla los argumentos al programa
- if ((argc != 2) || ((N = atoi(argv[1])) <= 0) ) {
+  //Controla los argumentos al programa
+  if ((argc != 2) || ((N = atoi(argv[1])) <= 0) ){
     printf("\nUsar: %s n\n  n: Dimension de la matriz (nxn X nxn)\n", argv[0]);
     exit(1);
   }
 
- 
- //Aloca memoria para las matrices
+
+ // Alocación de memoria para las matrices
+ int TRIANGULAR_SIZE = ((N*N)+N) / 2;
+ printf("%i\n", N);
+
  A=(double*)malloc(sizeof(double)*N*N);
- BT=(double*)malloc(sizeof(double)*N*N);
+ BT=(double*)malloc(sizeof(double)*TRIANGULAR_SIZE);
  C=(double*)malloc(sizeof(double)*N*N);
- //Esta es la matraiz triangular en forma de vector guardando todo menos los ceros
- Prueba=(double*)malloc(sizeof(double)*tamTriangular);
+
 
  //Inicializa las matrices
  //La matriz A se inicializa todas las columnas en 0 menos la ultima
- //La matriz BT se inicializa triangular inferior con 1
+ //La matriz BT se inicializa triangular inferior con N
  //Esto permite que el resultado sean todos 1
  //La matriz C se inicializa en 0
  for(i=0;i<N;i++){
@@ -59,51 +64,44 @@ int main(int argc,char* argv[]){
     A[i*N+j]=1;
    }
 
-   if(i>=j){
-    BT[j*N+i]=N;
-   }
-   
+ 
    //Para cargar la matriz triangular en forma de vector
-   if(i>=j){
-    Prueba[i - calcularCeros(j) + N*j]= i - calcularCeros(j) + N*j;
+   if(i >= j){
+    int index = i - cantidad_ceros(j) + N*j;
+    BT[index] = N;
    }
 
    C[i*N+j]=0;  
   }
  }
 
- //Realiza la multiplicacion 
- timetick = dwalltime();
+  timetick = dwalltime();
 
 
- for(i=0;i<N;i++){
-  for(j=0;j<N;j++){
-   for(k=0;k<N;k++){
+  // Codigo optimizado:
+  // - Se encarga de recorrer las matrices sin iterar sobre los ceros de la matriz triangular
+  for(i=0; i<N; i++){
 
-    // Si está arriba de la diagonal, entonces el valor de B[k,j] es 0
-    // C ya está inicializada en 0, entonces salteo las operaciones en diagonal
+    for(j=0; j<N; j++){
 
-    // Esto evita:
-    // - 1 asignación
-    // - 2 operaciones Punto Flotante = 1 suma y 1 multiplicación
-    // - Los 4 accesos a memoria (asignación + operadores)
+      // Notar que solo recorre las diagonales
 
-    if (k >= j){
-      C[i*N+j] = C[i*N+j] + A[i*N+k] * BT[j*N+k];
+
+      // for(k=0; k<(N-j); k++){
+
+      //   C[i*N+j] = C[i*N+j] + A[i*N+k+j] * BT[k+j+j*N];
+
+      // }
+
+      for(k=0; k<N; k++)
+      {
+        if (k >= j){
+          int index = k - cantidad_ceros(j) + N*j;
+          C[i*N+j] = C[i*N+j] + A[i*N+k] * BT[index];
+        }
+      }
     }
-
-   }
-  }
  }
-
-  //Este codigo es el que se encarga de recorrer las matrices sin iterar sobre los ceros de la matriz triangular
-  //for(i=0;i<N;i++){
-  //for(j=0;j<N;j++){
-  //for(k=0;k<N-j;k++){
-    //C[i*N+j]=C[i*N+j] + A[i*N+k+j]*BT[k+j+j*N];
-   //}
-  //}
- //}
 
  printf("Tiempo en segundos %f\n", dwalltime() - timetick);
 
