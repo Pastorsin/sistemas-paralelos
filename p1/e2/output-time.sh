5a7039254e0fea2af83f9c2c3c8ff3dc4ae7dc3f
Andrés Milla <andressmilla@gmail.com>
2020-03-26 10:50:28 -0300

    Add .out to gitignore

--- p1/e2/output-time.sh
+++ p1/e2/output-time.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gcc -o multBloques multBloques.c
+gcc -o multBloques.out multBloques.c
 if [ ! -d "outputs" ]; then
   mkdir outputs
 fi
-cp multBloques outputs
+cp multBloques.out outputs
 
 cd outputs
 echo " ================= Algoritmo con Bloques =================== "
@@ -15,7 +15,7 @@ do
   for (( cantBloques=1; cantBloques<=matriz; cantBloques=cantBloques*2 ))
   do
     echo " -------------------------- Cantidad bloques -> $cantBloques Tamano bloque -> $tamBloque"
-    ./multBloques $cantBloques $tamBloque 0
+    ./multBloques.out $cantBloques $tamBloque 0
     tamBloque=$((tamBloque/2))
   done
 done
