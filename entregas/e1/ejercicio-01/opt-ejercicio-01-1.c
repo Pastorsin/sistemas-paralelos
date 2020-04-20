74789225ff7fb00904730b7f41bf61e7a890a59d
Andrés Milla <andressmilla@gmail.com>
2020-04-20 16:02:14 -0300

    Arreglado ejercicio 1 de la entrega 1

--- entregas/e1/ejercicio-01/opt-ejercicio-01-1.c
+++ entregas/e1/ejercicio-01/opt-ejercicio-01-1.c
@@ -1,99 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define ORDENXFILAS 0
-#define ORDENXCOLUMNAS 1
 
 //Dimension por defecto de las matrices
-int N=100;
-
-//Retorna el valor de la matriz en la posicion fila y columna segun el orden que este ordenada
-double getValor(double *matriz,int fila,int columna,int orden){
-  if(orden==ORDENXFILAS){
-    return(matriz[fila*N+columna]);
-  } else {
-    return(matriz[fila+columna*N]);
-  }
-}
-
-//Establece el valor de la matriz en la posicion fila y columna segun el orden que este ordenada
-void setValor(double *matriz,int fila,int columna,int orden,double valor){
- if(orden==ORDENXFILAS){
-  matriz[fila*N+columna]=valor;
- }else{
-  matriz[fila+columna*N]=valor;
- }
-}
+int N = 100;
 
 //Para calcular tiempo
-double dwalltime(){
+double dwalltime() {
   double sec;
   struct timeval tv;
 
-  gettimeofday(&tv,NULL);
-  sec = tv.tv_sec + tv.tv_usec/1000000.0;
+  gettimeofday(&tv, NULL);
+  sec = tv.tv_sec + tv.tv_usec / 1000000.0;
   return sec;
 }
 
-int main(int argc,char*argv[]){
-  double *A,*B,*C;
-  int i,j,k;
-  int check=1;
+int main(int argc, char*argv[]) {
+  double *A, *B, *C;
+  int i, j, k;
+  int check = 1;
   double timetick;
 
   //Controla los argumentos al programa
-  if ((argc != 2) || ((N = atoi(argv[1])) <= 0) ){
+  if ((argc != 2) || ((N = atoi(argv[1])) <= 0) ) {
     printf("\nUsar: %s n\n  n: Dimension de la matriz (nxn X nxn)\n", argv[0]);
     exit(1);
   }
 
   //Aloca memoria para las matrices
-  A=(double*)malloc(sizeof(double)*N*N);
-  B=(double*)malloc(sizeof(double)*N*N);
-  C=(double*)malloc(sizeof(double)*N*N);
+  A = (double*)malloc(sizeof(double) * N * N);
+  B = (double*)malloc(sizeof(double) * N * N);
+  C = (double*)malloc(sizeof(double) * N * N);
 
   //Inicializa las matrices A y B en 1, el resultado sera una matriz con todos sus valores en N
-  for(i=0;i<N;i++){
-   for(j=0;j<N;j++){
-     setValor(A,i,j,ORDENXFILAS,1);
-     setValor(B,i,j,ORDENXCOLUMNAS,1);
-   }
-  }   
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      A[i * N + j] = 1;
+      B[j * N + i] = 1;
+    }
+  }
 
 
- //Realiza la multiplicacion
+  //Realiza la multiplicacion
 
   timetick = dwalltime();
 
-  for(i=0;i<N;i++){
-   for(j=0;j<N;j++){
-    double suma_parcial = 0;
-    
-    for(k=0;k<N;k++){
-      suma_parcial += getValor(A,i,k,ORDENXFILAS)*getValor(B,k,j,ORDENXCOLUMNAS);
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      double suma_parcial = 0;
+
+      for (k = 0; k < N; k++) {
+        suma_parcial += A[i * N + k] * B[j * N + k];
+      }
+
+      C[i * N + j] = suma_parcial;
     }
+  }
 
-    setValor(C,i,j,ORDENXFILAS,suma_parcial);
-   }
-  }   
-
-  printf("Multiplicacion de matrices de %dx%d. Tiempo en segundos %f\n",N,N, dwalltime() - timetick);
+  printf("Multiplicacion de matrices de %dx%d. Tiempo en segundos %f\n", N, N, dwalltime() - timetick);
 
   //Verifica el resultado
-  for(i=0;i<N;i++){
-    for(j=0;j<N;j++){
-      check=check&&(getValor(C,i,j,ORDENXFILAS)==N);
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      check = check && C[i * N + j];
     }
-  }   
+  }
 
-  if(check){
+  if (check) {
     printf("Multiplicacion de matrices resultado correcto\n");
-  }else{
+  } else {
     printf("Multiplicacion de matrices resultado erroneo\n");
   }
 
- free(A);
- free(B);
- free(C);
+  free(A);
+  free(B);
+  free(C);
 
- return(0);
+  return (0);
 }
