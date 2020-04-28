7b70f9549365875ebf4975c9740f63b872d74be4
Andrés Milla <andressmilla@gmail.com>
2020-04-28 15:57:59 -0300

    Agrreglada solución de ejercicio 2 entrega 1

--- entregas/e1/ejercicio-02/ejercicio-02.c
+++ entregas/e1/ejercicio-02/ejercicio-02.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-//Dimension por defecto de las matrices
+
+// Dimension por defecto de las matrices
 int N = 100;
 
-//Para calcular tiempo
+
+// Para calcular tiempo
 double dwalltime() {
   double sec;
   struct timeval tv;
@@ -14,10 +16,24 @@ double dwalltime() {
   return sec;
 }
 
-int main(int argc, char*argv[]) {
-  double *A, *B, *C;
-  int i, j, k;
+
+// Chequea si el resultado de la matriz resultante es N*N
+int resultado_valido(double *M) {
   int check = 1;
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      check = check && (M[i * N + j] == N * N);
+    }
+  }
+
+  return check;
+}
+
+
+int main(int argc, char*argv[]) {
+  double *A, *B, *C, *AB, *D;
+  int i, j, k;
   double timetick;
 
   //Controla los argumentos al programa
@@ -30,42 +46,62 @@ int main(int argc, char*argv[]) {
   A = (double*)malloc(sizeof(double) * N * N);
   B = (double*)malloc(sizeof(double) * N * N);
   C = (double*)malloc(sizeof(double) * N * N);
+  AB = (double*)malloc(sizeof(double) * N * N);
+  D = (double*)malloc(sizeof(double) * N * N);
 
-  //Inicializa las matrices A y B en 1, el resultado sera una matriz con todos sus valores en N
+
+  // Inicializa las matrices A, B y C en 1
+  // El resultado sera una matriz con todos sus valores en N*N
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
       A[i * N + j] = 1;
       B[j * N + i] = 1;
+      C[j * N + i] = 1;
     }
   }
 
+  printf("Dimensión de las matrices: %ix%i\n\n", N, N);
 
-  //Realiza la multiplicacion
+  printf("Realizando multiplicación D=ABC . . .\n\n");
 
   timetick = dwalltime();
 
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < N; j++) {
+  // Realiza el producto AB=A.B
+  // AB está organizada por filas
+  // A está organizada por filas
+  // B está organizada por columnas
+  for ( i = 0; i < N; i++) {
+
+    for ( j = 0; j < N; j++) {
       double suma_parcial = 0;
 
-      for (k = 0; k < N; k++) {
+      for ( k = 0; k < N; k++) {
         suma_parcial += A[i * N + k] * B[j * N + k];
       }
 
-      C[i * N + j] = suma_parcial;
+      AB[i * N + j] = suma_parcial;
     }
   }
 
-  printf("Multiplicacion de matrices de %dx%d. Tiempo en segundos %f\n", N, N, dwalltime() - timetick);
+  // Realiza el producto D=AB.C
+  // D está organizada por filas
+  // AB está organizada por filas
+  // C está organizada por columnas
+  for ( i = 0; i < N; i++) {
 
-  //Verifica el resultado
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < N; j++) {
-      check = check && C[i * N + j];
+    for ( j = 0; j < N; j++) {
+      double suma_parcial = 0;
+
+      for ( k = 0; k < N; k++) {
+        suma_parcial += AB[i * N + k] * C[j * N + k];
+      }
+
+      D[i * N + j] = suma_parcial;
     }
   }
+  printf("Tiempo en segundos %f\n", dwalltime() - timetick);
 
-  if (check) {
+  if (resultado_valido(D)) {
     printf("Multiplicacion de matrices resultado correcto\n");
   } else {
     printf("Multiplicacion de matrices resultado erroneo\n");
@@ -74,6 +110,8 @@ int main(int argc, char*argv[]) {
   free(A);
   free(B);
   free(C);
+  free(AB);
+  free(D);
 
   return (0);
 }
